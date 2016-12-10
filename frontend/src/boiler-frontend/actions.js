90b95b882fc80494f1bacaba3bf50dc07a954eb1
Kai Davenport <kaiyadavenport@gmail.com>
2016-12-10 18:28:44 +0000

    remove temp modules

--- frontend/src/boiler-frontend/actions.js
+++
@@ -1,6 +0,0 @@
-export const toggle_menu = (open = true) => {
-  return {
-    type: 'BOILER_TOGGLE_MENU',
-    open
-  }
-}
--- frontend/src/boiler-frontend/selectors.js
+++
@@ -1,1 +0,0 @@
-export const isMenuOpen = (state) => state.boiler.isMenuOpen
