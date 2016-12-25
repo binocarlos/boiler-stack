e5e77fe6444281d0e628062972b72d50e681d650
Kai Davenport <kaiyadavenport@gmail.com>
2016-12-25 14:35:09 +0000

    fix dev paths for serving index.html

--- frontend/server.js
+++ frontend/server.js
@@ -6,6 +6,7 @@ const config = require('./webpack.config.dev');
 const app = express();
 const compiler = webpack(config);
 
+const APPS = ['app', 'admin']
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath,
@@ -16,25 +17,20 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler))
 
+app.use(function(req, res, next){
+  const foundapp = APPS.filter(function(app){
+    return (req.url == '/' + app) || (req.url.indexOf('/' + app + '/') == 0)
+  })[0]
+  if(!foundapp) return next()
+  res.sendFile(path.join(__dirname, './www/' + foundapp + '/index.html'));
+})
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, './www/index.html'));
 })
+// this is a production file only
 app.get('/vendor.js', function (req, res) {
   res.end('')
-})
-app.get('/app', function (req, res) {
-  res.sendFile(path.join(__dirname, './www/app/index.html'));
-})
-
-app.get('/admin', function (req, res) {
-  res.sendFile(path.join(__dirname, './www/admin/index.html'));
-})
-app.get('/app/*', function (req, res) {
-  res.sendFile(path.join(__dirname, './www/app/index.html'));
-})
-
-app.get('/admin/*', function (req, res) {
-  res.sendFile(path.join(__dirname, './www/admin/index.html'));
 })
 
 app.listen(process.env.PORT || 80, '0.0.0.0', function (err) {
