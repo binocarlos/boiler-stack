53d083e36b18e11660280b312aa38cf56bb559f0
Kai Davenport <kaiyadavenport@gmail.com>
2017-01-03 10:41:16 +0000

    wrap mongocrud constructor

--- frontend/src/app_user/apis.js
+++ frontend/src/app_user/apis.js
@@ -3,6 +3,13 @@ import { MongoCodec } from '../boiler-ui/lib/api/codecs'
 import UserApis from '../boiler-ui/lib/plugins/user/apis'
 import URLS from './config/urls'
 
+const MongoCrud = (opts = {}) => {
+  return CrudAjax(Object.assign({}, opts, {
+    encode: MongoCodec.encode,
+    decode: MongoCodec.decode
+  }))
+}
+
 export const user = UserApis({
   status: () => URLS.user.status,
   login: () => URLS.user.login,
@@ -10,11 +17,9 @@ export const user = UserApis({
   update: () => URLS.user.update
 })
 
-export const installation = CrudAjax({
+export const installation = MongoCrud({
   name: 'installation',
-  getUrl: () => URLS.installation,
-  encode: MongoCodec.encode,
-  decode: MongoCodec.decode
+  getUrl: () => URLS.installation
 })
 
 const apis = {
