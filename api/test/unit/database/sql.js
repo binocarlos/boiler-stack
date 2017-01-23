75d7a5e7d219c6ab044874d2e985e2ef9b674bba
Kai Davenport <kaiyadavenport@gmail.com>
2017-01-23 12:50:29 +0000

    refactor sql test for later non dupe

--- api/test/unit/database/sql.js
+++ api/test/unit/database/sql.js
@@ -1,20 +1,24 @@
 const tape = require('tape')
 const SQL = require('../../../src/database/sql')
 
+const BASE_WHERE = {
+  a: 'hello',
+  b: 'apples'
+}
+
+const BASE_SQL = [
+  '"a" = $1',
+  'and',
+  '"b" = $2'
+].join("\n")
+
+const BASE_PARAMS = Object.keys(BASE_WHERE).map(k => BASE_WHERE[k])
+
 tape('sql - where', (t) => {
-
-  const query = SQL.where({
-    a: 'hello',
-    b: 'apples'
-  })
-
+  const query = SQL.where(BASE_WHERE)
   t.deepEqual(query, {
-    sql:[
-      '"a" = $1',
-      'and',
-      '"b" = $2'
-    ].join("\n"),
-    params: ['hello', 'apples']
+    sql:BASE_SQL,
+    params: BASE_PARAMS
   }, 'the query is correct')
 
   t.end()
