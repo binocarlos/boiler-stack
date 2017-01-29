3fd7cce786f07b2891f159be5f5a8df6e8d11f8f
Kai Davenport <kaiyadavenport@gmail.com>
2017-01-29 14:55:06 +0000

    all acceptance tests passing again

--- api/test/acceptance/installations.js
+++ api/test/acceptance/installations.js
@@ -10,18 +10,15 @@ tape('acceptance - installations', (t) => {
 
   async.series({
 
-    register: (next) => tools.register(userData, next)/*,
+    register: (next) => tools.register(userData, next),
     pause: (next) => setTimeout(next, 300),
     installations: (next) => tools.installations(next),
-    status: (next) => tools.status(next)*/
+    status: (next) => tools.status(next)
 
   }, (err, results) => {
 
     if(err) t.error(err)
 
-    console.log(JSON.stringify(results, null, 4))
-
-  /*
     const register = results.register
     const installations = results.installations
     const status = results.status
@@ -30,11 +27,11 @@ tape('acceptance - installations', (t) => {
     t.equal(installations.body[0].name, 'My First Company', 'default installation')
 
     t.equal(status.body.data.meta.activeInstallation, installations.body[0].id, 'the user the active installation')
-*/
+
     t.end()
   })
 })
-/*
+
 tape('acceptance - create installation', (t) => {
   const userData = tools.UserData()
   const INSTALLATION_NAME = 'apples install'
@@ -316,4 +313,4 @@ tape('acceptance - access control - no user', (t) => {
     
     t.end()
   })
-})*/
+})
